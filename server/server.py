b07ce51cf44889a6694e5b38db5ff8452784af77
razfrumkin <raz.frumkin@gmail.com>
2023-12-16 20:32:31 +0200

    Made search players feature

--- server/server.py
+++ server/server.py
@@ -7,7 +7,34 @@ import nba_api.stats.endpoints.playercareerstats
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/endpoints/career-stats')
+@app.route('/endpoints/search', methods=['GET'])
+def search():
+    prompt = request.args.get('prompt', type=str)
+
+    teams_filter_mode = request.args.get('teamsFilterMode', 'none')
+    if teams_filter_mode not in {'none', 'all'}: teams_filter_mode = 'none'
+
+    players_filter_mode = request.args.get('playersFilterMode')
+    if players_filter_mode not in {'none', 'all', 'activeOnly'}: players_filter_mode = 'none'
+
+    ids = search_data(prompt, teams_filter_mode, players_filter_mode)
+    return(ids)
+
+def search_data(prompt: str, teams_filter_mode: str, players_filter_mode: str) -> dict:
+    teams = nba_api.stats.static.teams.find_teams_by_full_name(prompt) if teams_filter_mode != 'all' else []
+    players = nba_api.stats.static.players.find_players_by_full_name(prompt) if players_filter_mode != 'all' else []
+    if players_filter_mode == 'activeOnly':
+        players = [player for player in players if player['is_active']]
+
+    teams_ids = [team['id'] for team in teams]
+    players_ids = [player['id'] for player in players]
+
+    return {
+        'teams_ids': teams_ids,
+        'players_ids': players_ids
+    }
+
+@app.route('/endpoints/career-stats', methods=['GET'])
 def get_career_stats():
     player_id = request.args.get('playerId', type=int)
     data = nba_api.stats.endpoints.playercareerstats.PlayerCareerStats(player_id=player_id)
@@ -19,12 +46,12 @@ def get_career_stats():
 
     return jsonify(stats)
 
-@app.route('/endpoints/teams')
+@app.route('/endpoints/teams', methods=['GET'])
 def get_teams():
     teams = nba_api.stats.static.teams.get_teams()
     return jsonify(teams)
 
-@app.route('/endpoints/players')
+@app.route('/endpoints/players', methods=['GET'])
 def get_players():
     players = nba_api.stats.static.players.get_players()
     return jsonify(players)
